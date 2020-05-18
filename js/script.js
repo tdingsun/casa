d31fe742ab82ecc2b19a607de63b112709041170
Hongxia Sun <Hong@Sunans-MBP.home>
2020-05-18 02:02:03 -0700

    models

--- js/script.js
+++ js/script.js
@@ -12,14 +12,62 @@ var mouseX = 0, mouseY = 0;
 
 var windowHalfX = window.innerWidth / 2;
 var windowHalfY = window.innerHeight / 2;
-// var objFileNames = ['MA_Arbol', 'MA_Flor', 'Piedra1', 'Monumento3-1', 'Monumento4', 'Piedra1'];
-var objFileNames = ['ARBOL2V2'];
+var objFileNames = ['Monumento4', 'Piedra1', 'Monumento2', 'Flor2', 'Arbol3', 'ARBOL2V2', 'mONUMENTO3', 'Arbol4', 'Monumento1', 'Planta1', 'Piedra2', 'Monumento5', 'Flor1'];
+objFileNames = shuffle(objFileNames);
 
 var views = {
+	'Monumento4': {
+		"position": [-248.52, -123.126, 416.96],
+		"rotation": [0.27, -0.54, 0.14]
+	},
+	'Piedra1': {
+		"position": [-4.43, 0.34, 42.96],
+		"rotation": [-0.01, -0.21, 0.0]
+	},
+	'Monumento2': {
+		"position": [23.36, -12.50, 22.95],
+		"rotation": [0.35, 0.31, -0.11]
+	},
+	'Flor2': {
+		"position": [0.57, 7.69, -0.21],
+		"rotation": [-1.57, -7.00, -2.37]
+	},
+	'Arbol3': {
+		"position": [37.47, 69.83, 39.33],
+		"rotation": [-1.56, 0.00, -0.60]
+	},
 	'ARBOL2V2': {
 		"position": [-16.64, 1.95, -33.21],
 		"rotation": [-3.12, -0.19, -3.13]
-	}
+	},
+	'mONUMENTO3': {
+		"position": [-0.85, -5.00, -9.98],
+		"rotation": [-2.81, 0.42, 3.00]
+	},
+	'Arbol4': {
+		"position": [-0.86, 5.00, -11.50],
+		"rotation": [-2.89, -0.60, -2.99]
+	},
+	'Monumento1': {
+		"position": [-67.90, 6.72, 85.92],
+		"rotation": [-0.07, -0.63, -0.04]
+	},
+	'Planta1': {
+		"position": [50.81, 3.28, 6.53],
+		"rotation": [-0.47, 1.42, 0.46]
+	},
+	'Piedra2': {
+		"position": [-7.82, 4.06, 64.87],
+		"rotation": [-6.19, -0.15, -9.58]
+	},
+	'Monumento5': {
+		"position": [22.98, -31.39, 9.89],
+		"rotation": [1.89, 0.66, -2.06]
+	},
+	'Flor1': {
+		"position": [-10, 48.01, 0.00],
+		"rotation": [-1.57, -6.51, 0.00]
+	},
 }
 
 var objID = 0;
@@ -56,6 +104,11 @@ function init() {
 	controls.movementSpeed = 5;
 	controls.lookSpeed = 0.01;
 
+	//orbit controls
+	// controls = new OrbitControls(camera, renderer.domElement);
+	// controls.addEventListener('change', render);
+
+
 	//scene
 
 	scene = new THREE.Scene();
@@ -75,10 +128,11 @@ function init() {
 
 	//material
 	material = new THREE.MeshPhongMaterial({
-		shininess: 100,
-		color: 0xaaaaaa,
-		emissive: 0x555555,
-		specular: 0xeeeeee
+		shininess: 150,
+		color: 0xbbbbbb,
+		emissive: 0x333333,
+		specular: 0xaaaaaa,
+		flatShading: true
 	});
 
 
@@ -89,6 +143,7 @@ function init() {
 
 	function loadNextFile(){
 		if(objID >= objFileNames.length) return;
+
 		let name = objFileNames[objID]
 		loader.load(`./models/${name}.gltf`, function(o){
 			if(obj != null){
@@ -123,17 +178,14 @@ function init() {
 
 			objID += 1;
 			// controls.update();
+
 			camera.position.set(views[name].position[0], views[name].position[1], views[name].position[2]);
 			camera.rotation.set(views[name].rotation[0], views[name].rotation[1], views[name].rotation[2]);
 			let vLookAt = new THREE.Vector3(0, 0, -1).applyQuaternion(camera.quaternion).add(camera.position);
 			controls.lookAt(vLookAt);
-			// let fwd = new THREE.Vector3(0,0,1);
-			// fwd.applyMatrix3(camera.matrixWorld).normalize();
-			// let offset = fwd.multiplyScalar(100);
-			// camera.position.set(controls.target).sub(offset);
 			render();
 
-			setTimeout(loadNextFile, 5000);
+			setTimeout(loadNextFile, 2000);
 
 		});
 	}
@@ -145,8 +197,6 @@ function init() {
 
 
 
-	// controls = new OrbitControls(camera, renderer.domElement);
-	// controls.addEventListener('change', render);
 	// // controls.enableDamping = true;
 	// // controls.dampingFactor = 0.05;
 	// controls.screenSpacePanning = false;
@@ -186,7 +236,6 @@ function onDocumentClick( event ) {
 	console.log(camera.position);
 	console.log(camera.rotation);
 }
-//
 
 function animate() {
 
@@ -195,22 +244,27 @@ function animate() {
 
 }
 
-var t = 0;
-var r = 250;
-var xdelta = 0;
-var zdelta = 0;
-
 function render() {
-
-	// t += 0.01;
-
-	// xdelta = r * Math.cos(t);
-	// zdelta = r * Math.sin(t);
-	// camera.position.x = r * Math.cos(mouseX * 0.05);
-	// camera.position.z = r * Math.sin(mouseX * 0.05);
-	// camera.position.y = (-mouseY + windowHalfY);
-	// camera.lookAt( scene.position );
 	controls.update( clock.getDelta() );
 	renderer.render( scene, camera );
+}
 
+function shuffle(array) {
+
+	var currentIndex = array.length;
+	var temporaryValue, randomIndex;
+
+	// While there remain elements to shuffle...
+	while (0 !== currentIndex) {
+		// Pick a remaining element...
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+
+		// And swap it with the current element.
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
+
+	return array;
 }
